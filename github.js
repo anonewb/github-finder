9e4c1568655dee1c8273593f9f26387f54845437
ano <anonewbe@gmail.com>
2018-06-12 12:26:25 +0530

    fetching and displaying repos

--- github.js
+++ github.js
@@ -3,15 +3,21 @@ class Github {
     // get these from github/settings/developer settings/oauth apps
     this.client_id = '82be063fd01c9d416e7e';
     this.client_secret = 'cc3c8a26ffb4bcb9f6ab8ddcad8ad9df7fc2b8e0';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json(); // profile data
+    const repos = await reposResponse.json(); // repos data
 
     return {
-      profile //same as profile: profile
+      profile, //same as profile: profile
+      repos
     }
   }
 }
